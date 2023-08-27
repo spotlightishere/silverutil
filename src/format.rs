27dd715a0808bd45e70aedcad086936e8347ae46
Spotlight <spotlight@joscomputing.space>
2023-08-27 16:19:10 -0500

    Only use binread
    `binwrite` currently provides very little help for offset-oriented formats like ours, and largely does not function. A future commit will migrate away from `binrw` entirely.
    
    It's still an excellent crate - if your project can support it, absolutely worth a use!

--- src/format.rs
+++ src/format.rs
@@ -1,8 +1,18 @@
-use binrw::{args, binrw, io::SeekFrom};
+use binrw::{args, binread, io::SeekFrom};
 
-#[binrw]
-#[brw(little)]
+/// Simple helper to assist with seeking from start with a u32.
+fn seek_start(offset: u32) -> SeekFrom {
+    SeekFrom::Start(offset.into())
+}
+
+#[binread]
+#[br(little)]
 pub struct SilverDBFormat {
+    // When writing, we need to pass the total header length, and section count.
+    #[bw(args {
+        calc_section_count: 0,
+        calc_header_length: 0,
+    })]
     pub header: SilverDBHeader,
 
     // When reading, we need to pass on the header length to sections
@@ -11,55 +21,68 @@ pub struct SilverDBFormat {
     pub sections: Vec<SectionHeader>,
 }
 
-#[binrw]
+#[binread]
 pub struct SilverDBHeader {
-    // 0x03 across 5th, 6th, and 7th generation iPod nanos.
+    /// 0x03 across 5th, 6th, and 7th generation iPod nanos.
     pub version: u32,
-    // The length consumed by header content.
-    // Resource data begins immediately after all header values.
+
+    /// The length consumed by header content.
+    /// Resource data begins immediately after all header values.
     pub header_length: u32,
-    // The amount of sections this database contains.
+
+    /// The amount of sections this database contains.
     pub section_count: u32,
 }
 
 /// Simply a u32, but we read it as an array to ensure endianness.
 pub type SectionMagic = [u8; 4];
 
-#[binrw]
+#[binread]
 #[br(import {
     header_length: u32
 })]
 pub struct SectionHeader {
-    // The magic identifying this section (i.e. 'Str ', 'BMap', 'LDTm', etc.)
+    /// The magic identifying this section (i.e. 'Str ', 'BMap', 'LDTm', etc.)
     pub magic: SectionMagic,
-    // The amount of resources contained within this section.
+    /// The amount of resources contained within this section.
     pub resource_count: u32,
     /// Whether this section's IDs are sequential and start at 1. Necessary for certain resource types such as `StrT`.
     pub is_sequential: u32,
-    // Offset to array of resource entries, relative to the start of the file (0x0).
+    /// Offset to array of resource entries, relative to the start of the file (0x0).
     pub resource_offset: u32,
 
     // Similar to SilverDBFormat, we need to pass on the header length to
-    // sections so that resource content location can be determined.
-    #[br(args { inner: args! { header_length } }, count = resource_count, seek_before = SeekFrom::Start(resource_offset.into()), restore_position)]
+    // sections so that resource content location can be determined while reading.
+    #[br(
+        args { inner: args! { header_length } },
+        count = resource_count,
+        seek_before = seek_start(resource_offset),
+        restore_position
+    )]
+    /// All available resources within this section.
     pub resources: Vec<ResourceMetadata>,
 }
 
-#[binrw]
+#[binread]
 #[br(import {
     header_length: u32
 })]
 pub struct ResourceMetadata {
-    // The ID is how this resource is referenced. For example, 0x0dad06d8.
+    /// The ID is how this resource is referenced. For example, 0x0dad06d8.
     pub id: u32,
-    // The offset to where this resource's data is located.
-    // This is relative to where data begins (i.e. after header, section header, and resource entries.)
+    /// The offset to where this resource's data is located.
+    /// This is relative to where data begins (i.e. after header, section header, and resource entries.)
     pub data_offset: u32,
-    // The length of this resource.
+    /// The length of this resource.
     pub data_size: u32,
 
-    // Again, our data is relative to where the header ends.
+    // When we're reading, we know our data is relative to where the header ends.
     // We know this location via the passed `header_length` argument.
-    #[br(seek_before = SeekFrom::Start((header_length + data_offset).into()), count=data_size, restore_position)]
+    #[br(
+        seek_before = seek_start(header_length + data_offset),
+        count = data_size,
+        restore_position
+    )]
+    /// The raw, binary contents of this resource.
     pub contents: Vec<u8>,
 }
